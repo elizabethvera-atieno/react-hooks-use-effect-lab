458de6b2e2e22e18bc01776061cefe1c25874da0
elizabethvera-atieno <atienolizvera@gmail.com>
2022-05-19 14:45:21 +0300

    complete test

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,7 +3,23 @@ import React, { useState } from "react";
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+ useEffect(()=>{
+  if (timeRemaining===0){
+    setTimeRemaining(10);
+    onAnswered(false);
+    return;
+  }
+
+  const timeID = setTimeout(()=>{
+    setTimeRemaining((timeRemaining)=>timeRemaining - 1)
+  }, 1000);
+
+
+  return funcion ();{
+    clearTimeout(timeID);
+  }
+
+  },[timeRemaining, onAnswered])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
